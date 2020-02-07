4ecfd8a5327422a662a81a7e1267519d04cb40b9
Bianca Nicole <bnbasilio@gmail.com>
2020-02-07 06:25:10 +0000

    10 submitted

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers 00 to 99
+ * Return: 0
+ */
+int main(void)
+{
+	int first = 0;
+	int second  = 0;
+
+	for (first = 0; first < 10; first++)
+	{
+		for (second = 0; second < 10; second++)
+		{
+			putchar(48 + first);
+			putchar(48 + second);
+			if (first != 9 || second != 9)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
+
