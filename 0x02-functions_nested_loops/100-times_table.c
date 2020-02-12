825731a2c560b42d5310e054ed64ad583fef42a9
Bianca Nicole <bnbasilio@gmail.com>
2020-02-12 07:50:48 +0000

    12 submitted, non betty compliant

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: maximum multiplier in the times table
+ * Return: none
+ */
+void print_times_table(int n)
+{
+	int a;
+	int b;
+	int c;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+	else
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+				if (b == 0)
+				{
+					_putchar('0');
+				}
+				else if (c < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + c);
+				}
+				else if (c < 100 && c >= 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + c / 10);
+					_putchar('0' + c % 10);
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar('0' + c / 100);
+					_putchar('0' + (c % 100) / 10);
+					_putchar('0' + c % 10);
+				}
+				if (b == n)
+				{
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
+
