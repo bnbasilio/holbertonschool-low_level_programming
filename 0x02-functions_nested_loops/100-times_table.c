9213d15f6ee2f59122d7c458447aab9c3ea8bf2e
Bianca Nicole <bnbasilio@gmail.com>
2020-02-13 06:43:42 +0000

    Added commas to output

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <unistd.h>
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: maximum multiplier in the times table
@@ -16,23 +17,30 @@ void print_times_table(int n)
 			{
 				c = a * b;
 				if (b == 0)
+				{
 					_putchar('0');
+				}
 				else if (c < 10)
 				{
+					_putchar(',');
+					write(1, "   ", 3);
+					/*_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar(' ');*/
 					_putchar('0' + c);
 				}
 				else if (c < 100 && c >= 10)
 				{
-					_putchar(' ');
-					_putchar(' ');
+					_putchar(',');
+					write(1, "  ", 2);
+					/*_putchar(' ');
+					_putchar(' ');*/
 					_putchar('0' + c / 10);
 					_putchar('0' + c % 10);
 				}
 				else
 				{
+					_putchar(',');
 					_putchar(' ');
 					_putchar('0' + c / 100);
 					_putchar('0' + (c % 100) / 10);
