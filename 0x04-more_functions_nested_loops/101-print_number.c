aa050f3a5f6a89808c39a1e71cc8410d0268d7f0
Bianca Nicole <bnbasilio@gmail.com>
2020-02-14 07:33:38 +0000

    Added conditional for 0

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <math.h>
 /**
  * print_number - prints an integer
  * @n: integer to be printed
@@ -7,13 +6,19 @@
  */
 void print_number(int n)
 {
-	int m, count, divisor, digit;
-
-	if (-n == n / -1)
+	int m, count, divisor, digit, i;
+	
+	i = 0;
+	m = n;
+	if (n < 0)
 	{
 		m = -n;
 		_putchar('-');
 		n = n / -1;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
 	}
 	for (count = 0; n > 0; count++)
 	{
@@ -21,7 +26,11 @@ void print_number(int n)
 	}
 	while (m > 0)
 	{
-		divisor = pow(10, (count - 1));
+		divisor = 1;
+		for (i = 0; i < (count - 1); i++)
+		{
+			divisor *= 10;
+		}
 		digit = m / divisor;
 		_putchar('0' + digit);
 		m = m - (digit * divisor);
