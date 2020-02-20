2c9a7a556391dac8b9b4ba1ec6aad9ff893995b1
Bianca Nicole <bnbasilio@gmail.com>
2020-02-20 07:20:39 +0000

    10 submitted

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ *_atoi - converts a string to an integer
+ *@s: string input
+ *Return: integer after conversion
+ */
+int _atoi(char *s)
+{
+	int n, i;
+	char *beg = s;
+
+	n = 0;
+	while (*s < '0' || *s > '9')
+	{
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		s++;
+	}
+	s--;
+	for (i = 1; *s >= '0' && *s <= '9'; i *= 10)
+	{
+		n += *s * i;
+		s--;
+	}
+	while (s >= beg)
+	{
+		if (*s == '-')
+		{
+			n = -n;
+		}
+		else
+		{
+			s--;
+		}
+	}
+	return (n);
+}
+
