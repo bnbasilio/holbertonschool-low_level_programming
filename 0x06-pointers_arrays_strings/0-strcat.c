89f8569b1205d01f756c057b5a4c5d88d519b5bd
Bianca Nicole <bnbasilio@gmail.com>
2020-02-20 18:09:20 +0000

    0 submitted

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *_strcat - concatenates two strings
+ *@dest: string as to which src is appended to
+ *@src: string to be appended to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
