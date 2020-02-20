048f18359900e67dc38e79624bc39f010c2f89a1
Bianca Nicole <bnbasilio@gmail.com>
2020-02-20 19:14:41 +0000

    3 submitted

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *_strcmp - compares two strings
+ *@s1: first string
+ *@s2: second string
+ *Return: dest
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int n;
+
+	n = *s1 - *s2;
+	return (n);
+}
+
