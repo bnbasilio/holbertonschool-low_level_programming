166d22e3f061e24ca9fa1ee1c9d23fc88f91d131
Bianca Nicole <bnbasilio@gmail.com>
2020-02-20 21:17:31 +0000

    5 submitted

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ **string_toupper - changes all lowercase letters of a string to uppercase
+ *@s: string to be converted
+ *Return: converted string
+ */
+char *string_toupper(char *s)
+{
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'z')
+		{
+			*s -= 32;
+		}
+		s++;
+	}
+	return (s);
+}
+
