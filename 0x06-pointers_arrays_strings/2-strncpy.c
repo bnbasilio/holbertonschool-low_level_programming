86c27f253e2c80818d8b7058ffed5794bde94f04
Bianca Nicole <bnbasilio@gmail.com>
2020-02-20 19:26:47 +0000

    2 submitted

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ **_strncpy - copies a string
+ *@dest: string as to which src is copied to
+ *@src: string to be copied to dest
+ *@n: max number of characters to be used from src
+ *Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	/*while (dest[i])
+	{
+		i++;
+	}*/
+	for (i = 0; i < n && src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
+
