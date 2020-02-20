4c7eb444852cde9dbeb9a3eef111cf3f2743465b
Bianca Nicole <bnbasilio@gmail.com>
2020-02-20 18:40:22 +0000

    1 submitted

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ **_strncat - concatenates two strings grabbing max of n characters from src
+ *@dest: string as to which src is appended to
+ *@src: string to be appended to dest
+ *@n: max number of characters to be used from src
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, m;
+
+	i = 0;
+	j = 0;
+	m = 0;
+	while (dest[i])
+	{
+		i++;
+	}
+	for (m = 0; m < n && src [j]; m++)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
