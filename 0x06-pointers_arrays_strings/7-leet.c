e304446af9924e3cc65290b0f33c3f9aae2f9937
Bianca Nicole <bnbasilio@gmail.com>
2020-02-20 21:54:50 +0000

    7 submitted

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ **leet - encodes a string into 1337
+ *@s: string to be encoded
+ *Return: s (encoded string)
+ */
+char *leet(char *s)
+{
+	while (*s)
+	{
+		if (*s == 'a' || *s == 'A')
+		{
+			*s = '4';
+		}
+		else if (*s == 'e' || *s == 'E')
+		{
+			*s = '3';
+		}
+		else if (*s == 'o' || *s == '0')
+		{
+			*s = '0';
+		}
+		else if (*s == 't' || *s == 'T')
+		{
+			*s = '7';
+		}
+		else if (*s == 'l' || *s == 'L')
+		{
+			*s = '1';
+		}
+		s++;
+	}
+	return (s);
+}
+
