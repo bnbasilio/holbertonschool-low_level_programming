36c86864a359f95a7a153b52b7186cd1bcb756df
Bianca Nicole <bnbasilio@gmail.com>
2020-02-21 07:44:18 +0000

    8 submitted

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ **rot13 - encodes a string using rot13
+ *@s: string to be encoded
+ *Return: s (encoded string)
+ */
+char *leet(char *s)
+{
+	int i = 0;
+	char *ptr_s = s;
+	char *encodeThis = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *translateTo = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*ptr_s)
+	{
+		for (i = 0; encodeThis[i]; i++)
+		{
+			if (*ptr_s == encodeThis[i])
+			{
+				*ptr_s = translateTo[i];
+			}
+		}
+		ptr_s++;
+	}
+	return (s);
+}
+
