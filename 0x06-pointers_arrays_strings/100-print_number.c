71e10bd71acc0030e60b6f6446da88467761b33e
Bianca Nicole <bnbasilio@gmail.com>
2020-02-21 18:57:12 +0000

    9 submitted

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	int m, count, divisor, digit, i, j, x;
+
+	i = 0;
+	m = n;
+	if (n < 0)
+	{
+		m = -n;
+		_putchar('-');
+		n = n / -1;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	for (count = 0; n > 0; count++)
+	{
+		n = n / 10;
+	}
+	x = count;
+	for (j = 0; j < x; j++)
+	{
+		divisor = 1;
+		for (i = 0; i < (count - 1); i++)
+		{
+			divisor *= 10;
+		}
+		digit = m / divisor;
+		_putchar('0' + digit);
+		m = m - (digit * divisor);
+		count--;
+	}
+}
+
