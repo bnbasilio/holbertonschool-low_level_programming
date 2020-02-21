b1d681023430e1f4128a9b210662cbbd2e7f102b
Bianca Nicole <bnbasilio@gmail.com>
2020-02-21 07:18:25 +0000

    corrected capitalization logic

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ **cap_string - capitalizes all words of a string
+ *@s: string to be converted
+ *Return: converted string
+ */
+char *cap_string(char *s)
+{
+	int capNext;
+	char *sep =	" \t\n,;.!?\"(){}";
+	char *ptr_sep = sep;
+	char *t = s;
+	
+	capNext = 1;
+	while (*s)
+	{
+		if (capNext)
+		{
+			if (*s >= 'a' && *s <= 'z')
+			{
+				*s -= 32;
+			}
+		}
+		for (ptr_sep = sep; *ptr_sep; ptr_sep++)
+		{
+			if (*s == *ptr_sep)
+			{
+				capNext = 1;
+				break;
+			}
+			capNext = 0;
+		}
+		s++;
+	}
+	return (t);
+}
+
