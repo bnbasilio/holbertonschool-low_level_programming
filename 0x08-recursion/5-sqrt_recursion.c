33bd6e906da18a90cbefba111a3ca728c8ec0507
Bianca Nicole <bnbasilio@gmail.com>
2020-02-26 21:20:25 +0000

    made 5 betty compliant

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- *_sqrt_recursion - calculates the natural square root of a number
+ *sqrt_rec - calculates the square root of numbers 2 and larger
  *@n: number whose square root needs to be calculated
  *Return: natural square root of n, -1 otherwise
  */
@@ -12,6 +12,11 @@ int sqrt_rec(int n, int sqrt)
 		return (-1);
 	return (sqrt_rec(n, sqrt + 1));
 }
+/**
+ *_sqrt_recursion - calculates the natural square root of a number
+ *@n: number whose square root needs to be calculated
+ *Return: natural square root of n, -1 otherwise
+ */
 int _sqrt_recursion(int n)
 {
 	if (n == 0 || n == 1)
