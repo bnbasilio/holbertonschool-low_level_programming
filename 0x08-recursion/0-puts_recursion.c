7ad13fe4e36ecd0befdf5cc686d48204fc03c469
Bianca Nicole <bnbasilio@gmail.com>
2020-02-26 17:14:31 +0000

    0 submitted

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *_puts_recursion - prints a string
+ *@s: string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*s == '\0')
+		_putchar('\n');
+}
+
