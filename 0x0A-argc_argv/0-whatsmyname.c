4c52a044d2927afc9f941f95c8cea53037a4d563
Bianca Nicole <bnbasilio@gmail.com>
2020-03-01 03:08:10 +0000

    made 0 betty compliant

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-
+/**
+ *main - prints the program's name
+ *@argc: number of arguments passed to the program
+ *@argv: array of arguments passed to the program
+ *
+ *Return: 0 if successful
+ */
 int main(int argc, char *argv[])
 {
-	printf ("%s\n", argv[argc - 1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
 
