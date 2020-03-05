99f3b99d06adf42479ba8f0f8a9b2d34f4f661f2
Bianca Nicole <bnbasilio@gmail.com>
2020-03-05 20:19:22 +0000

    task 2 completed

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_calloc - allocates memory for an array and sets memory to 0
+ *@nmemb: number of elements
+ *@size: number of bytes for each element
+ *Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	for (i = 0; i < nmemb; i++)
+	{
+		arr[i] = 0;
+	}
+	return (arr);
+}
