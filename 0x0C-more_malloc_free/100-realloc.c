69185d35aec05b70cf772ec8c9cf3a4698250f45
Bianca Nicole <bnbasilio@gmail.com>
2020-03-06 18:38:54 +0000

    task 4 completed

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_realloc - reallocates a memory block using malloc and free
+ *@ptr: pointer to the memory previously allocated with a call to malloc
+ *@old_size: size of the allocated space for ptr
+ *@new_size: new size of the new memory block
+ *
+ *Return: pointer to allocated memory
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int *arr;
+	unsigned int i;
+	
+	if (old_size == new_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	arr = malloc(sizeof(char) * new_size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < old_size; i++)
+	{
+		arr[i] = *((char*)ptr + i);
+	}
+	free(ptr);
+	return(arr);
+}
