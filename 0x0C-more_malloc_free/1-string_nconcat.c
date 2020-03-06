ace4977c7ff346cb152f5b3b8656f8ed17fa4b73
Bianca Nicole <bnbasilio@gmail.com>
2020-03-06 00:40:26 +0000

    initialized length to 0 in _strlen

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		str[i] = s1[i];
 	}
-	for (j = 0; j < n; i++, j++)
+	for (j = 0; s2[j] && j < n; i++, j++)
 	{
 		str[i] = s2[j];
 	}
@@ -47,6 +47,7 @@ int _strlen(char *s)
 {
 	unsigned int length, i;
 
+	length = 0;
 	for (i = 0; s[i]; i++)
 	{
 		length++;
