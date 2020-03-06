8d25dd3d29dca925cc44b466d4566a072349e11b
Bianca Nicole <bnbasilio@gmail.com>
2020-03-06 18:48:39 +0000

    corrected dereference to void pointer

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,7 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	for (i = 0; i < old_size; i++)
 	{
-		arr[i] = *((char *)ptr + i);
+		*((char *)arr + i) = *((char *)ptr + i);
 	}
 	free(ptr);
 	return (arr);
