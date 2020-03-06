d9e8c146f43b8199a92348872116d6b0d37769ba
Bianca Nicole <bnbasilio@gmail.com>
2020-03-06 00:11:57 +0000

    moved if statement up

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,15 +15,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
 	unsigned int i, j, s1len, s2len;
-
-	s1len = _strlen(s1);
-	s2len = _strlen(s2);
-	if (n >= s2len)
-		n = s2len;
+	
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	s1len = _strlen(s1);
+	s2len = _strlen(s2);
+	if (n >= s2len)
+		n = s2len;
 	str = malloc(sizeof(char) * (s1len + n + 1));
 	if (str == NULL)
 		return (NULL);
