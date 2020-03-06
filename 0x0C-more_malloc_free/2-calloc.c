034e80ed2dc3a6216bc0399fec980b4752f4f10f
Bianca Nicole <bnbasilio@gmail.com>
2020-03-06 00:49:25 +0000

    added condition for malloc failing

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,11 +11,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arr;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < nmemb; i++)
 	{
 		arr[i] = 0;
