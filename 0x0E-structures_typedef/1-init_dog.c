3f5019b5c34c7d564bb75b81d36b547380b494cb
Bianca Nicole <bnbasilio@gmail.com>
2020-03-10 02:55:46 +0000

    made 1 betty compliant

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,17 +1,18 @@
 #include "dog.h"
 /**
  *init_dog - initializes a varible of type struct dog
- *@dog: pointer to dog
+ *@d: pointer to dog
  *@name: name of dog
  *@age: age of dog
  *@owner: dog owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	 if (d)
-	 {
+	if (d)
+	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-	 }
+	}
 }
+
