a51418f12de9c58e86c8b3b01c986ee089959e15
Bianca Nicole <bnbasilio@gmail.com>
2020-03-10 05:32:23 +0000

    task 4 completed

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,61 @@
+#include "dog.h"
+#include <stdlib.h>
+char *_strdup(char *str);
+/**
+ *new_dog - creates a new dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: dog owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doge;
+
+	doge = malloc(sizeof(dog_t));
+	if (doge == NULL)
+		return (NULL);
+	doge->name = _strdup(name);
+	if (doge->name == NULL)
+	{
+		free(doge);
+		return (NULL);
+	}
+	doge->age = age;
+	doge->owner = _strdup(owner);
+	if (doge->owner == NULL)
+	{
+		free(doge->name);
+		free(doge);
+		return (NULL);
+	}
+	return (doge);
+}
+
+/**
+ *_strdup - returns a pointer to the copy of the string used as parameter
+ *@str: string to be copied
+ *
+ *Return: a pointer to the newly allocated memory space of a string's copy
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i, length;
+
+	length = 0;
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+	{
+		length++;
+	}
+	s = malloc(sizeof(char) * (length + 1));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+	{
+		s[i] = str[i];
+	}
+	s[i] = '\0';
+	return (s);
+}
