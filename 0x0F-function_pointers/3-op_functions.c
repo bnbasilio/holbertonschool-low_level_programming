8b5e1f22854f8368cd8047fa873c4528eecc9a5c
Bianca Nicole <bnbasilio@gmail.com>
2020-03-11 20:12:52 +0000

    operator functions completed

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,46 @@
+/**
+ *op_add - adds 2 integers
+ *@a: first integer
+ *@b: second integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - subtracts 2 integers
+ *@a: first integer
+ *@b: second integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+ /**
+ *op_mul - multiplies 2 integers
+ *@a: first integer
+ *@b: second integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - returns the result of the division of a by b
+ *@a: first integer
+ *@b: second integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_mod - returns the remainder of the division of a by b
+ *@a: first integer
+ *@b: second integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
