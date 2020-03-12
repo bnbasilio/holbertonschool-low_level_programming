8196a7a622c4c17497be9a2e3d4bf62291a44e98
Bianca Nicole <bnbasilio@gmail.com>
2020-03-12 04:22:02 +0000

    task 3 initial commit

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,6 +2,7 @@
  *op_add - adds 2 integers
  *@a: first integer
  *@b: second integer
+ *Return: sum of a and b
  */
 int op_add(int a, int b)
 {
@@ -11,15 +12,17 @@ int op_add(int a, int b)
  *op_sub - subtracts 2 integers
  *@a: first integer
  *@b: second integer
+ *Return: difference between a and b
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
- /**
+/**
  *op_mul - multiplies 2 integers
  *@a: first integer
  *@b: second integer
+ *Return: product of a and b
  */
 int op_mul(int a, int b)
 {
@@ -29,6 +32,7 @@ int op_mul(int a, int b)
  *op_div - returns the result of the division of a by b
  *@a: first integer
  *@b: second integer
+ *Return: the quotient of a and b
  */
 int op_div(int a, int b)
 {
@@ -38,6 +42,7 @@ int op_div(int a, int b)
  *op_mod - returns the remainder of the division of a by b
  *@a: first integer
  *@b: second integer
+ *Return: the remainder
  */
 int op_mod(int a, int b)
 {
