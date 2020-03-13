dd8d868ccd4e69b254790e40d53293e3976a58cb
bnbasilio <bnbasilio@gmail.com>
2020-03-13 18:09:54 +0000

    added struct for task #3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+/**
+ *struct format - format specifiers and corresponding data type
+ *@spec: format specifier
+ *@type: data type associated
+ */
+typedef struct format
+{
+	char *spec;
+	char *type;
+} form_spec;
 
 #endif /* VARIADIC_FUNCTIONS_H */
