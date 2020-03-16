3fdb5140b86dcc8b7d0321adbeeb1aa77a51511f
bnbasilio <bnbasilio@gmail.com>
2020-03-16 05:58:01 +0000

    fixed tabs and spaces

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -7,11 +7,11 @@ section .text
 
 global main
 main:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, message
-    mov rdx, msg_len
-    syscall
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, msg_len
+	syscall
 	mov rax, 60
 	mov rdi, 0
 	syscall
