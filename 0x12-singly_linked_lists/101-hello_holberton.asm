d5e0802e62b53f898bd0d8fa66ad79e37dc118d9
bnbasilio <bnbasilio@gmail.com>
2020-03-20 05:57:46 +0000

    task 6 initial commit

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+extern printf
+
+section .data
+	msg: db "Hello, Holberton", 10, 0
+	fmt: db "%s", 17, 0
+
+section .text
+
+global main
+main:
+	push	rbp
+
+	mov		rdi,fmt
+	mov		rsi,msg
+	mov		rax,0
+	call    printf
+
+	pop		rbp
+
+	mov		rax,0
+	ret
