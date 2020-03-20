8a6220f43872073e3b097f75cd9c34d614f0d3fa
bnbasilio <bnbasilio@gmail.com>
2020-03-20 05:36:08 +0000

    task 5 initial commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ *b4_main - prints before main is executed
+ */
+
+void b4_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
