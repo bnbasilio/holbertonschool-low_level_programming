4017fbda71ec3d7dc9d070b9063531234da15f35
bnbasilio <bnbasilio@gmail.com>
2020-03-20 06:04:46 +0000

    fixed formatting

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,18 @@
-extern printf
+	extern printf
 
-section .data
-	msg: db "Hello, Holberton", 10, 0
-	fmt: db "%s", 16, 0
-section .text
-global main
+	section .data
+msg: db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
 
+	section .text
+
+	global main
 main:
 	push	rbp
-
 	mov		rdi,fmt
 	mov		rsi,msg
 	mov		rax,0
 	call    printf
-
 	pop		rbp
-
 	mov		rax,0
 	ret
