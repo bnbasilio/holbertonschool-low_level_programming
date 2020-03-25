0568f0d2d8c429ffe83796ce78edbf5a6248d9aa
bnbasilio <bnbasilio@gmail.com>
2020-03-25 05:10:07 +0000

    rearranged for loop

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,9 +22,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (i = 0; i < idx - 1; i++)
 	{
-		if (current == NULL)
+		if (current != NULL)
+			current = current->next;
+		else
 			return (NULL);
-		current = current->next;
 	}
 
 	new_node = malloc(sizeof(listint_t));
