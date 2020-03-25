2fe0d52fb907861aaf3f867993ec206544d7a4d2
bnbasilio <bnbasilio@gmail.com>
2020-03-25 05:17:30 +0000

    last attempt

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	new_node = malloc(sizeof(listint_t));
-	if (!new)
+	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
