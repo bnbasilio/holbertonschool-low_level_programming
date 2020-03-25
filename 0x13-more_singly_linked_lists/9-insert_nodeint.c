09ff185ffa9b085befbaff3fed2719bb40de2d0b
bnbasilio <bnbasilio@gmail.com>
2020-03-25 05:05:01 +0000

    added else statement to final conditional

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,8 +37,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node->next = *head;
 		*head = new_node;
 	}
-	new_node->next = current->next;
-	current->next = new_node;
+	else
+	{
+		new_node->next = current->next;
+		current->next = new_node;
+	}
 
 	return (new_node);
 }
