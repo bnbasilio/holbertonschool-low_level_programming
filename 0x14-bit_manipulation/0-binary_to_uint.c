94276b3313c89ca93f41811920fad6df5b5d13d6
bnbasilio <bnbasilio@gmail.com>
2020-03-27 05:08:55 +0000

    made betty compliant

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- *binary to uint - converts a binary number to an unsigned int
+ *binary_to_uint - converts a binary number to an unsigned int
  *@b: pointer to a string of 0 and 1 chars
  *Return: the converted number, otherwise 0
  */
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	for (exp = 0; b[exp]; exp++)
 	{
 		if (b[exp] < '0' || b[exp] > '1')
-			return 0;
+			return (0);
 	}
 	exp--;
 
